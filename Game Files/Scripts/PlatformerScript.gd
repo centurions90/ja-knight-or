202f521ed196e7f438e3c5d13eea02d8dca6489f
centurions90 <chrisaaron.cheng@gmail.com>
2020-02-01 14:18:58 +0800

    added state machine for player

--- Game Files/Scripts/PlatformerScript.gd
+++ Game Files/Scripts/PlatformerScript.gd
@@ -15,6 +15,8 @@ export(float) var fall_acceleration = 2200
 var floor_normal = Vector2(0, -1)
 var linear_vel = Vector2()
 
+onready var anim_tree = $AnimationTree
+
 func _physics_process(delta):
 	# Move and slide
 	linear_vel = move_and_slide(linear_vel, floor_normal)
@@ -29,6 +31,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		target_speed += 1.0
 	
+	# Animation
+	if target_speed == 0:
+		anim_tree["parameters/conditions/is_idle"] = true
+		anim_tree["parameters/conditions/is_running"] = false
+	else:
+		anim_tree["parameters/conditions/is_idle"] = false
+		anim_tree["parameters/conditions/is_running"] = true
+	
 	# Acceleration and Deceleration
 	var x_acceleration = 0
 	if target_speed != 0:
@@ -38,13 +48,23 @@ func _physics_process(delta):
 	linear_vel.x = lerp(linear_vel.x, target_speed * walk_speed, x_acceleration)
 
 	# Jumping
+	anim_tree["parameters/conditions/on_floor"] = on_floor
+	
 	if on_floor and Input.is_action_just_pressed("jump"):
 		linear_vel.y = -jump_height
+		anim_tree["parameters/conditions/is_jumping"] = true
 	
 	# Falling
 	if !on_floor and linear_vel.y < 0:
 		linear_vel.y += jump_deceleration * delta
 	elif linear_vel.y >= 0:
 		linear_vel.y += fall_acceleration * delta
+		anim_tree["parameters/conditions/is_jumping"] = false
+		anim_tree["parameters/conditions/is_falling"] = true
+	
+	if linear_vel.y == 0:
+		anim_tree["parameters/conditions/is_falling"] = false
 	
 	linear_vel.y = min(linear_vel.y, fall_speed)
+	
+	### ATTACK ###
