54b741d41012d67803b5c7705287178175322583
centurions90 <chrisaaron.cheng@gmail.com>
2020-02-01 16:35:17 +0800

    finished state machine for platforming animation

--- Game Files/Scripts/PlatformerScript.gd
+++ Game Files/Scripts/PlatformerScript.gd
@@ -16,6 +16,8 @@ var floor_normal = Vector2(0, -1)
 var linear_vel = Vector2()
 
 onready var anim_tree = $AnimationTree
+onready var sprite = $Pivot/Sprite
+onready var pivot = $Pivot
 
 func _physics_process(delta):
 	# Move and slide
@@ -28,8 +30,11 @@ func _physics_process(delta):
 	# Horizontal movement
 	if Input.is_action_pressed("move_left"):
 		target_speed -= 1.0
+		pivot.scale = Vector2(-1, 1)
+		
 	if Input.is_action_pressed("move_right"):
 		target_speed += 1.0
+		pivot.scale = Vector2(1, 1)
 	
 	# Animation
 	if target_speed == 0:
@@ -48,11 +53,11 @@ func _physics_process(delta):
 	linear_vel.x = lerp(linear_vel.x, target_speed * walk_speed, x_acceleration)
 
 	# Jumping
-	anim_tree["parameters/conditions/on_floor"] = on_floor
 	
 	if on_floor and Input.is_action_just_pressed("jump"):
 		linear_vel.y = -jump_height
 		anim_tree["parameters/conditions/is_jumping"] = true
+		anim_tree["parameters/conditions/on_floor"] = false
 	
 	# Falling
 	if !on_floor and linear_vel.y < 0:
@@ -61,9 +66,16 @@ func _physics_process(delta):
 		linear_vel.y += fall_acceleration * delta
 		anim_tree["parameters/conditions/is_jumping"] = false
 		anim_tree["parameters/conditions/is_falling"] = true
+		anim_tree["parameters/conditions/on_floor"] = false
 	
-	if linear_vel.y == 0:
+	if on_floor:
 		anim_tree["parameters/conditions/is_falling"] = false
+		anim_tree["parameters/conditions/on_floor"] = true
+	
+	if (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") and on_floor):
+		anim_tree["parameters/conditions/roll"] = true
+	else:
+		anim_tree["parameters/conditions/roll"] = false
 	
 	linear_vel.y = min(linear_vel.y, fall_speed)
 	
